55e0034a64da4425c945738b0055d8ef710e0528
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-10-02 00:13:03 +0900

    프로젝트 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}", // src 폴더 내부의 모든 관련 파일
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
